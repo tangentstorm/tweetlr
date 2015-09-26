0a63b525be668ee43231794d1bf47c60e6330b3f
tangentstorm <michal.wallace@gmail.com>
2015-09-26 17:47:15 -0400

    added tables for users and tweets, then some initial data

--- cfg/create-db.sql
+++ cfg/create-db.sql
@@ -1,2 +1,34 @@
 create database if not exists tweetlr;
 grant all on tweetlr.* to 'tweetlr'@'localhost' identified by 'tweetlr';
+
+use tweetlr;
+
+create table if not exists users (
+   id integer not null auto_increment primary key,
+   username varchar(32),
+   passhash char(32)
+);
+
+create table if not exists tweets (
+  id integer not null auto_increment primary key,
+  user_id integer not null references users.id,
+  tweet varchar(140)
+);
+
+
+replace into users values
+  (1, 'admin', md5('admin')),
+  (2, 'samc', md5('Abner')),
+  (3, 'dparker', md5('alg0nqu1n')),
+  (4, 'groucho', md5('10021890'));
+
+
+replace into tweets values
+  ( 1, 2, 'Do the right thing. It will gratify some people and astonish the rest.'),
+  ( 2, 3, 'Brevity is the soul of lingerie.'),
+  ( 3, 2, 'All you need in this life is ignorance and confidence, then success is sure.'),
+  ( 4, 4, 'I''ve had a perfectly wonderful evening. But this wasn''t it.'),
+  ( 5, 3, 'There''s a hell of a distance between wise-cracking and wit. Wit has truth in it; wise-cracking is simply calisthenics with words.'),
+  ( 6, 2, 'If you tell the truth, you don''t have to remember anything.'),
+  ( 7, 3, 'If I didn''t care for fun and such, I''d probably amount to much. But I shall stay the way I am, because I do not give a damn.'),
+  ( 8, 4, 'The secret of life is honesty and fair dealing. If you can fake that, you''ve got it made.');
